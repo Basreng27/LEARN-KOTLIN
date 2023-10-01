3eb6b886e08d6bf6692037dd0d280c4afabdd240
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-01 09:00:09 +0700

    INITIAL COMMIT

---
+++ src/main/kotlin/com/learn_kotlin/App.kt
@@ -0,0 +1,12 @@
+// function Main (Fungsi Utama yang akan dijalankan)
+fun main(){
+//    mendeklarasikan varibale
+    val name = "Wandi"
+
+//    perbedaan print dengan println yaitu print tidak akan membuar baris baru untuk selanjutnya
+//    sedangkan println akan membuat baris baru setelahnya
+//    output
+    print("Name : ")
+    println(name)
+    println("Hello Kotlin")
+}

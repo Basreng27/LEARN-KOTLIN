34b695c9a58e4c23f3862381049f90a92bb9d052
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-07 12:22:13 +0700

    LEARN : If Condition

---
+++ src/main/kotlin/com/learn_kotlin/If.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val jamBuka = 7
+    val sekarang = 20
+    val kantor: String
+
+    // if
+    if (sekarang > jamBuka) {
+        kantor = "Buka"
+    } else if (sekarang == jamBuka) {
+        kantor = "Wait a minute, office will be open"
+    } else {
+        kantor = "Tutup"
+    }
+
+    println(kantor)
+}

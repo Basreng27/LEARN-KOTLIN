ceaf752aa8325f433615329c2323a72e7996c975
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-07 08:35:00 +0700

    LEARN : Tipe Data

---
+++ src/main/kotlin/com/learn_kotlin/TipeData.kt
@@ -0,0 +1,12 @@
+fun main(){
+    // pengidentifikasian variable
+    // var identifier: Type = initialization
+    // pembuatan variable di bagi 2 bisa menggunakan var atau val
+    // va seperti const dalam php. nilainya tidak bisa dirubah
+    var name: String = "Rayandra Wandi Marselana" //pembuatan variable
+    // boleh juga di buat seperti ini
+    // var name = "@Raywndii"
+    name = "Wandi"
+
+    println(name)
+}

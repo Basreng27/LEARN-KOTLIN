f4aabd37a462a44d2553237fb23cbe8021638e32
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-09 10:20:25 +0700

    LEARN : String Template

---
+++ src/main/kotlin/com/learn_kotlin/StringTemplate.kt
@@ -0,0 +1,15 @@
+fun main(){
+    // biasa di pakai
+    var text = "Rayandra"
+    var firstChar = text[0]
+
+    println("First character of $text is $firstChar")
+
+    // menggunakan +
+    val name = "Kotlin"
+    println("My name is " + name)
+
+    // menggunakan if
+    val hour = 7
+    print("Office ${if (hour > 7) "already close" else "is open"}")
+}

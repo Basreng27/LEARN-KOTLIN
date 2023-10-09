f17e9f52a59fd1182d6cdd8ac96b2a076cb919a1
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-09 09:42:56 +0700

    LEARN : Array

---
+++ src/main/kotlin/com/learn_kotlin/Array.kt
@@ -0,0 +1,21 @@
+fun main(){
+    /*
+    // untuk membuat array bisa menggunakan library arrayOf() dan array memliki beberapa fungsi yaitu get, set, size
+    // array dengan tipe tertentu
+    intArrayOf() : IntArray
+    booleanArrayOf() : BooleanArray
+    charArrayOf() : CharArray
+    longArrayOf() : LongArray
+    shortArrayOf() : ShortArray
+    byteArrayOf() : ByteArray
+    */
+
+    // membuat array
+    val array = arrayOf(1, 3, 5, 7)
+    val mixArray = arrayOf(1, 3, 5, 7 , "Dicoding" , true)
+    val intArray = intArrayOf(1, 3, 5, 7)
+
+    println(array)
+    println(mixArray)
+    println(intArray[2])
+}

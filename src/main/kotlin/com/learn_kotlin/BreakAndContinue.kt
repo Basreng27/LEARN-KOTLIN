36a5d2e71b0ddbba4713cecc8ba1fc8dec829bc0
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-16 19:05:03 +0700

    LEARN : Break And Continue

---
+++ src/main/kotlin/com/learn_kotlin/BreakAndContinue.kt
@@ -0,0 +1,30 @@
+fun main() {
+    // continue
+    val listOfInt = listOf(1, 2, 3, null, 5, null, 7)
+
+    for (i in listOfInt) {
+        if (i == null) continue
+        print(i)
+    }
+
+    // break
+    for (i in listOfInt) {
+        if (i == null) break
+        print(i)
+    }
+
+    // Pada Kotlin, sebuah expression dapat ditandai dengan sebuah label.
+    // Label pada Kotlin memiliki sebuah identifier yang diikuti dengan tanda @.
+    // Contoh dari sebuah label adalah foo@ atau bar@.
+    loop@ for (i in 1..10) {
+        println("Outside Loop")
+
+        for (j in 1..10) {
+            println("Inside Loop")
+            if ( j > 5) break@loop
+        }
+    }
+    // Pada kode diatas, break yang sudah ditandai dengan label akan dilompati ke titik awal proses perulangan
+    // yang sudah ditandai dengan label. break akan menghentikan proses perulangan terluar dari dalam
+    // proses perulangan, di mana break tersebut dipanggil.
+}

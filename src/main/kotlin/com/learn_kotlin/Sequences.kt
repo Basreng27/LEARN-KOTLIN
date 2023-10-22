e9d25f7488c9f8398e017e6fbcc8374044d5ca02
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2023-10-22 10:55:19 +0700

    LEARN : Squences

---
+++ src/main/kotlin/com/learn_kotlin/Sequences.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // mempercepat pwrhitungan 1 jt data
+    val list = (1..1000000).toList()
+    list.asSequence().filter { it % 5 == 0 }.map { it * 2 }.forEach { println(it) }
+
+    // Untuk membuat objek Sequence, kita bisa menggunakan fungsi yang tersedia pada standard library yaitu generateSequence().
+    val sequenceNumber = generateSequence(1) { it + 1 }
+    sequenceNumber.take(5).forEach { print("$it ") }
+}
